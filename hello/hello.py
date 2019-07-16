26b8d3fed46e3207d3899211a7299d9ab5764a00
Greg Price <gnprice@gmail.com>
2019-07-15 20:23:26 -0700

    Demonstrate pulling in a random library from PyPI.
    I chose the library for this demo like so:
    * Went to https://pypi.org/ and looked at "Trending projects".
    * Tried each one, using example code from its README.
    * Went with the first one that worked.
    
    In the sample that happened to give me today, this was the 4th
    on the list:
    
    * `cpp-demangle` was first, and failed at `pip install` time
      with an error about `setuptools_rust`.  I think it just doesn't
      build from source in a stock environment; I get the same error
      with `pip install .`, in a fresh venv, after cloning the source.
    
    * `pygrok` failed at import time, trying to use `__file__`.
      See indygreg/PyOxidizer#69.
    
    * `python-whois` failed at import time: imports `past`, which
      imports `lib2to3`, which uses `__file__`.
    
    * `area` works.

--- hello/hello.py
+++ hello/hello.py
@@ -1,2 +1,8 @@
+from area import area
+
 if __name__ == '__main__':
     print("Hello, world!")
+
+    # From the example in the library's README: https://pypi.org/project/area/
+    obj = {'type':'Polygon','coordinates':[[[-180,-90],[-180,90],[180,90],[180,-90],[-180,-90]]]}
+    print(area(obj))
